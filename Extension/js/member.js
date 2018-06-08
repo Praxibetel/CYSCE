ddf2cddc794fb19df0198522c8c0983a0c55293e
Noah <bradindvorak@gmail.com>
2018-06-07 22:35:43 -0400

    Forum Sass Update - Sass and light css updated - `member.js` cleaned up a bit - removed `notification.css` (already consolidated)

--- Extension/js/member.js
+++ Extension/js/member.js
@@ -1,10 +1,8 @@
 $("#profile_Stories > div > span").each(function() {
-    $(this).html('<a href="../story/' + encodeURIComponent($(this).text()).replace(/[-_.~"]/g, function(c) {
-            return '%' + c.charCodeAt(0).toString(16);
-        }).replace(/%/g, '~').toLowerCase() + '">' +
-        $(this).html() +
-        '</a>'
-    );
+    var e = $(this);
+    e.wrap($("<a></a>", {
+        href: `../story/${encodeURIComponent(e.text()).replace(/[-_.~"]/g, (c) => `%${c.charCodeAt(0).toString(16)}`).replace(/%/g, "~").toLowerCase()}`
+    }));
 }).addClass("unpublished");
 
 $("#profile_Posts > div, #profile_Posts > div *").contents().filter(function() {
